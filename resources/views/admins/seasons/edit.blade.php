64567ab7cfc6b4c77c2f09bea125536a1e1cecd7
Chris Vickers <chris@myshopmanager.com>
2017-09-04 22:01:49 -0400

    Adding weeks to seasons

--- resources/views/admins/seasons/edit.blade.php
+++ resources/views/admins/seasons/edit.blade.php
@@ -11,5 +11,54 @@
 @section('content')
 
 
+    <div class="row">
+
+        <div class="col-xs-12 col-sm-6">
+
+            <div class="panel panel-primary">
+
+                <div class="panel-heading clearfix">
+                    Season - {{ $season->year }}
+                </div>
+
+                <div class="panel-body">
+                    {{ Form::open(array('method' => 'patch', 'route' => array('admins.seasons.update', $season->id))) }}
+
+                    @include('seasons._form')
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Update Season</button>
+                    </div>
+
+                    {{ Form::close() }}
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+        <div class="col-xs-12 col-sm-6">
+
+            <div class="panel panel-success">
+
+                <div class="panel-heading clearfix">
+                    Weeks
+                </div>
+
+                <div class="panel-body">
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
 
 @stop
